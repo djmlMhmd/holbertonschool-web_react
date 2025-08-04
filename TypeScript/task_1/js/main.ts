eb7490f6fa4ba75af629789d03d80339dca99157
Djamaldine <djamaldine-mohamed@live.fr>
2025-08-04 11:26:35 +0200

    Refactor StudentClass to include methods for homework and display name

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,7 +19,27 @@ interface printTeacherFunction {
 	(firstName: string, lastName: string): string
 }
 
+class StudentClass {
+	firstName: string;
+	lastName: string;
 
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework(): string {
+		return 'Currently working'
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
+}
+
+export interface StudentConstructor {
+	new(firstName: string, lastName: string): StudentClass;
+}
 
 export interface StudentClassInterface {
 	firstName: string,
