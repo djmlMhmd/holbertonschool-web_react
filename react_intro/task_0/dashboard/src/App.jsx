3895d9ebf9c33adecf48b05cdbf8bd96459a9a3c
Djamal <djamaldine-mohamed@live.fr>
2026-02-10 11:20:42 +0100

    refactor: Update build and test configurations by migrating Vite to .mjs, adjusting Jest's file transformer, and removing module type from package.json.

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2025 - holberton School</p>
+        <p>Copyright {new Date().getFullYear()} - holberton School</p>
       </div>
     </>
   );
