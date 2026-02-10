dbd9af68ddaa16695b9c46741f45aca80fa050f9
Djamal <djamaldine-mohamed@live.fr>
2026-02-10 12:23:57 +0100

    Task 2: Implement utils, Notifications and update App structure

--- react_intro/task_1/dashboard/src/Notifications.jsx
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -3,7 +3,7 @@ import './Notifications.css';
 
 export default function Notifications() {
   return (
-    <div className="Notifications">
+    <div className="notification-items">
       <p>Here is the list of notifications</p>
     </div>
   );
