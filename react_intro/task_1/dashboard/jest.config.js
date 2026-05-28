a11fb0666eb5979c0d8a7b8ba712154e3780a1e8
Djamal <djamaldine-mohamed@live.fr>
2026-05-28 10:51:09 +0200

    Fix task_1 jest config: extract to jest.config.js, add --watchAll=false, root-level re-exports

---
+++ react_intro/task_1/dashboard/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/fileTransformer.js',
+  },
+};
