fb7ba75ec437203c4b72a9c2ed3eca2f77b1a871
Djamal <djamaldine-mohamed@live.fr>
2026-06-24 13:53:35 +0200

    Update Footer component to include a separator in copyright message for improved clarity

--- react_styling/task_3/dashboard/src/Footer/Footer.jsx
+++ react_styling/task_3/dashboard/src/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { getCurrentYear, getFooterCopy } from '../utils/utils';
 function Footer() {
     return (
         <footer className="App-footer mt-auto border-t-4 border-main-color py-4 text-center text-[0.8rem] italic">
-            <p>Copyright {getCurrentYear()} {getFooterCopy()}</p>
+            <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
         </footer>
     );
 }
