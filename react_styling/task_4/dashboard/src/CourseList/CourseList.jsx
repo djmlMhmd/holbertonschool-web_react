2991023d286521bfff0be6a609594a4a4923285d
Djamal <djamaldine-mohamed@live.fr>
2026-06-26 05:09:57 +0200

    Update CourseList component text size for improved readability
    - Changed text size in the CourseList component from 'text-xs' to 'text-sm' for better visibility.
    - Ensured consistent styling across different screen sizes to enhance user experience.

--- react_styling/task_4/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -6,7 +6,7 @@ function CourseList({ courses = [] }) {
         return (
             <div className="mx-auto my-20 w-[66%] min-[520px]:my-14 min-[520px]:w-[78%] min-[912px]:mb-12 min-[912px]:mt-24 min-[912px]:w-4/5">
                 <span className="sr-only">Available courses</span>
-                <table id="CourseList" className="w-full border-collapse text-xs min-[520px]:text-base">
+                <table id="CourseList" className="w-full border-collapse text-sm min-[520px]:text-base">
                     <thead>
                         <CourseListRow textFirstCell="No course available yet" isHeader={true} />
                     </thead>
@@ -17,7 +17,7 @@ function CourseList({ courses = [] }) {
 
     return (
         <div className="mx-auto my-20 w-[66%] min-[520px]:my-14 min-[520px]:w-[78%] min-[912px]:my-12 min-[912px]:flex min-[912px]:min-h-[22rem] min-[912px]:w-4/5 min-[912px]:items-center">
-            <table id="CourseList" className="w-full border-collapse text-xs min-[520px]:text-base">
+            <table id="CourseList" className="w-full border-collapse text-sm min-[520px]:text-base">
                 <thead>
                     <CourseListRow textFirstCell="Available courses" isHeader={true} />
                     <CourseListRow
