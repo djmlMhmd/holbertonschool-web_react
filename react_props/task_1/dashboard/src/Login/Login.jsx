e1f88a0597396c2a58fada72277f98f68c7a3ef4
Djamal <djamaldine-mohamed@live.fr>
2026-06-26 05:43:52 +0200

    Refactor CourseList component for improved text visibility and responsiveness
    - Updated text size in the CourseList component from 'text-xs' to 'text-sm' for enhanced readability.
    - Adjusted margins and ensured consistent styling across various screen sizes to improve user experience.

---
+++ react_props/task_1/dashboard/src/Login/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+import "./Login.css";
+
+function Login() {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const focusInput = (ref) => {
+        ref.current.focus();
+    };
+
+    return (
+        <>
+            <div className="App-body">
+                <p>Login to access the full dashboard</p>
+
+                <label onClick={() => focusInput(emailRef)} htmlFor="email">
+                    Email:
+                </label>
+                <input type="email" id="email" name="email" ref={emailRef} />
+
+                <label onClick={() => focusInput(passwordRef)} htmlFor="password">
+                    Password
+                </label>
+                <input type="password" id="password" name="password" ref={passwordRef} />
+
+                <button>OK</button>
+            </div>
+
+            <hr />
+        </>
+    );
+}
+
+export default Login;
