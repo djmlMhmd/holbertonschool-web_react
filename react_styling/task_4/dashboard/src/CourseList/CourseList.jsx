f3b799f323ba4431683d6b94ce4143cf8b5e4a61
Djamal <djamaldine-mohamed@live.fr>
2026-06-26 04:55:16 +0200

    Refactor CourseList component for improved layout and accessibility
    - Adjusted width and text size in the CourseList component for better responsiveness and readability.
    - Enhanced styling with Tailwind CSS to ensure a more consistent layout across different screen sizes.

--- react_styling/task_4/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -4,9 +4,9 @@ import CourseListRow from './CourseListRow';
 function CourseList({ courses = [] }) {
     if (courses.length === 0) {
         return (
-            <div className="mx-auto my-14 w-[78%] min-[912px]:mb-12 min-[912px]:mt-24 min-[912px]:w-4/5">
+            <div className="mx-auto my-14 w-[66%] min-[520px]:w-[78%] min-[912px]:mb-12 min-[912px]:mt-24 min-[912px]:w-4/5">
                 <span className="sr-only">Available courses</span>
-                <table id="CourseList" className="w-full border-collapse text-sm min-[520px]:text-base">
+                <table id="CourseList" className="w-full border-collapse text-xs min-[520px]:text-base">
                     <thead>
                         <CourseListRow textFirstCell="No course available yet" isHeader={true} />
                     </thead>
@@ -16,8 +16,8 @@ function CourseList({ courses = [] }) {
     }
 
     return (
-        <div className="mx-auto my-14 w-[78%] min-[912px]:my-12 min-[912px]:flex min-[912px]:min-h-[22rem] min-[912px]:w-4/5 min-[912px]:items-center">
-            <table id="CourseList" className="w-full border-collapse text-sm min-[520px]:text-base">
+        <div className="mx-auto my-14 w-[66%] min-[520px]:w-[78%] min-[912px]:my-12 min-[912px]:flex min-[912px]:min-h-[22rem] min-[912px]:w-4/5 min-[912px]:items-center">
+            <table id="CourseList" className="w-full border-collapse text-xs min-[520px]:text-base">
                 <thead>
                     <CourseListRow textFirstCell="Available courses" isHeader={true} />
                     <CourseListRow
